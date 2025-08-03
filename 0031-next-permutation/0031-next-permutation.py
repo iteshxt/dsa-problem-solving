c3c510421679a469dc39d917c12648ae12ecc4b8
Itesh Tomar <135621919+petrioteer@users.noreply.github.com>
2025-08-03 13:14:17 +0530

    Time: 0 ms (100%), Space: 17.8 MB (56.6%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.py
+++ 0031-next-permutation/0031-next-permutation.py
@@ -3,27 +3,22 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-# To find next permutations, we'll start from the end
-        i = j = len(nums)-1
-        # First we'll find the first non-increasing element starting from the end
-        while i > 0 and nums[i-1] >= nums[i]:
-            i -= 1
-        # After completion of the first loop, there will be two cases
-        # 1. Our i becomes zero (This will happen if the given array is sorted decreasingly). In this case, we'll simply reverse the sequence and will return 
-        if i == 0:
-            nums.reverse()
-            return 
-        # 2. If it's not zero then we'll find the first number grater then nums[i-1] starting from end
-        while nums[j] <= nums[i-1]:
-            j -= 1
-        # Now out pointer is pointing at two different positions
-        # i. first non-assending number from end
-        # j. first number greater than nums[i-1]
+        i = len(nums) - 2
+
+        while i >= 0 and nums[i] >= nums[i+1]:
+            i-=1
+
+        if i >= 0:
+
+            j = len(nums) - 1
+
+            while nums[j] <= nums[i]:
+
+                j -= 1
+
+            nums[i], nums[j] = nums [j], nums[i]
+
         
-        # We'll swap these two numbers
-        nums[i-1], nums[j] = nums[j], nums[i-1]
-        
-        # We'll reverse a sequence strating from i to end
-        nums[i:]= nums[len(nums)-1:i-1:-1]
-        # We don't need to return anything as we've modified nums in-place
-        
+        nums[i+1:] = reversed(nums[i+1:])
+
+        return nums
