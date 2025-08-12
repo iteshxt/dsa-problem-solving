6c90bc8889a6b614f9c314b6958e2e94d871ef16
Itesh Tomar <135621919+petrioteer@users.noreply.github.com>
2025-08-12 23:02:37 +0530

    Time: 3 ms (78.97%), Space: 17.9 MB (24.69%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.py
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -1,17 +1,14 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
-        roman_map = [
-            (1000, "M"), (900, "CM"), (500, "D"), (400, "CD"),
-            (100, "C"), (90, "XC"), (50, "L"), (40, "XL"),
-            (10, "X"), (9, "IX"), (5, "V"), (4, "IV"), (1, "I")
-        ]
+
+        values  = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+        symbols = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
         
-        result = []
+        roman = ""
         
-        for value, symbol in roman_map:
-            count, num = divmod(num, value)  # how many times this symbol fits
-            result.append(symbol * count)
-            if num == 0:  # early stop if fully converted
-                break
+        for i in range(len(values)):
+            while num >= values[i]:
+                roman += symbols[i]
+                num -= values[i]
         
-        return "".join(result)     
+        return roman
