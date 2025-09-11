889d9120b641494f5660fcb278d121c3dd30b7cf
Itesh Tomar <135621919+iteshxt@users.noreply.github.com>
2025-09-11 23:27:03 +0530

    Time: 65 ms (92.64%), Space: 20.5 MB (58.87%) - LeetHub

--- 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.py
+++ 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.py
@@ -1,16 +1,13 @@
 class Solution:
     def sortVowels(self, s: str) -> str:
-        vowels = 'aeiouAEIOU'
-        
-        # Extract and sort the vowels
-        vowel_chars = sorted([c for c in s if c in vowels])
-        
+        vowels_set = set("AEIOUaeiou")
+        vowels = [c for c in s if c in vowels_set]
+        vowels.sort()
+
         result = []
-        
+        it = iter(vowels)
+
         for c in s:
-            if c in vowels:
-                result.append(vowel_chars.pop(0))  # use the smallest available vowel
-            else:
-                result.append(c)
-        
+            result.append(next(it) if c in vowels_set else c)
+
         return ''.join(result)
