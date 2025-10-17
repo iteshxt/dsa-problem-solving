721b5eb69e5f904282767039414bb5744c5c96a9
Itesh Tomar <135621919+iteshxt@users.noreply.github.com>
2025-10-17 23:05:09 +0530

    Time: 1575 ms (5.17%), Space: 89.6 MB (48.28%) - LeetHub

---
+++ 3003-maximize-the-number-of-partitions-after-operations/3003-maximize-the-number-of-partitions-after-operations.py
@@ -0,0 +1,21 @@
+class Solution:
+  def maxPartitionsAfterOperations(self, s: str, k: int) -> int:
+    @functools.lru_cache(None)
+    def dp(i: int, canChange: bool, mask: int) -> int:
+      if i == len(s):
+        return 0
+
+      def getRes(newBit: int, nextCanChange: bool) -> int:
+        newMask = mask | newBit
+        if newMask.bit_count() > k:
+          return 1 + dp(i + 1, nextCanChange, newBit)
+        return dp(i + 1, nextCanChange, newMask)
+
+      res = getRes(1 << (ord(s[i]) - ord('a')), canChange)
+
+      if canChange:
+        for j in range(26):
+          res = max(res, getRes(1 << j, False))
+      return res
+
+    return dp(0, True, 0) + 1
