d6ee84b826a263daa5bdda4c232c9142eb92c51d
Itesh Tomar <135621919+iteshxt@users.noreply.github.com>
2025-11-10 20:44:38 +0530

    Time: 133 ms (99.38%), Space: 29.6 MB (90.63%) - LeetHub

--- 3542-minimum-operations-to-convert-all-elements-to-zero/3542-minimum-operations-to-convert-all-elements-to-zero.py
+++ 3542-minimum-operations-to-convert-all-elements-to-zero/3542-minimum-operations-to-convert-all-elements-to-zero.py
@@ -1,9 +1,13 @@
 class Solution:
-    def minOperations(self, nums: List[int]) -> int:
-        ops = 0
-        prev = 0
-        for x in nums:
-            if x > prev:
-                ops += x - prev
-            prev = x
-        return ops
+    def minOperations(self, nums):
+        stack = [0] * (len(nums) + 1)
+        top = 0
+        ans = 0
+        for num in nums:
+            while stack[top] > num:
+                top -= 1
+                ans += 1
+            if stack[top] != num:
+                top += 1
+                stack[top] = num
+        return ans + top
