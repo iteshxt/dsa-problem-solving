915961552932e6ad8a72e87210d0d6b411544290
Itesh Tomar <135621919+iteshxt@users.noreply.github.com>
2025-11-13 22:19:23 +0530

    Time: 59 ms (67.07%), Space: 17.8 MB (99.4%) - LeetHub

--- 3228-maximum-number-of-operations-to-move-ones-to-the-end/3228-maximum-number-of-operations-to-move-ones-to-the-end.py
+++ 3228-maximum-number-of-operations-to-move-ones-to-the-end/3228-maximum-number-of-operations-to-move-ones-to-the-end.py
@@ -1,12 +1,10 @@
 class Solution:
     def maxOperations(self, s: str) -> int:
-        zeros = 0
-        ops = 0
-        
-        for ch in reversed(s):
-            if ch == '0':
-                zeros += 1
-            else:  
-                ops += zeros
-        
-        return ops
+        ones = 0
+        res = 0
+        for i, c in enumerate(s):
+            if c == '1':
+                ones += 1
+            elif i > 0 and s[i - 1] == '1':
+                res += ones
+        return res
